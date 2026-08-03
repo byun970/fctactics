4a5dee3f68f96c19f91ce7babec34e5a091564e3
byun970 <byun970@gmail.com>
2026-08-03 15:44:19 +0900

    react-router 설정 / shadcn 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import "./App.css";
+import { Routes, Route, Outlet } from "react-router";
+import IndexPage from "./pages/index-page";
+import SignInPage from "./pages/sign-in-page";
+import SignUpPage from "./pages/sign-up-page";
 
-const App = () => {
-  return <div>App</div>;
-};
+function AuthLayout() {
+  return (
+    <div>
+      <header>Auth!</header>
+      <Outlet />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
+    </Routes>
+  );
+}
 
 export default App;
